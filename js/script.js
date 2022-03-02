54b4703285113ca75c86b2c6c119846f3bd7ccd4
Paula Golczyk <pp.golczyk@gmail.com>
2022-03-02 12:17:22 +0100

    blog

--- js/script.js
+++ js/script.js
@@ -3,24 +3,47 @@
     console.log('links:', links);
 }); */
   
+
 const titleClickHandler = function(event){
-    console.log(event);
-  
-    /* remove class 'active' from all article links  */
-  
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
-  
+    event.preventDefault();
+    const clickedElement = this;
+    console.log('Link was clicked!');
+ 
+    /* [DONE] remove class 'active' from all article links  */
+ 
+    const activeLinks = document.querySelectorAll('.titles a.active');
+ 
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
+   
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+
+    console.log('clickedElement:', clickedElement);
+     
+    /* [DONE] remove class 'active' from all articles */
+    const articles = document.querySelectorAll('.post');
+   
+    for(let article of articles){
+        article.classList.remove('active');
+    }
+
     /* get 'href' attribute from the clicked link */
-  
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
+ 
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+     const targetArticle = document.querySelector(articleSelector);
+     console.log(targetArticle);
+ 
     /* add class 'active' to the correct article */
+     targetArticle.classList.add('active');
   }
-  
+ 
   const links = document.querySelectorAll('.titles a');
-  
+ 
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
